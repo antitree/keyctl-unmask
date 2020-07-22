bb9d31a72f4280a5e2a1a9f5b7b2b217a5b62e7c
antitree <antitree@gmail.com>
2020-07-22 12:59:58 -0400

    cleaning up structure of code

--- cmd/keyctl-unmask/keyctl-unmask.go
+++ cmd/keyctl-unmask/keyctl-unmask.go
@@ -8,268 +8,13 @@ import (
 	"os"
 	"os/user"
 	"strings"
-	"syscall"
-	"unsafe"
 
 	"github.com/cheggaaa/pb"
-)
-
-const (
-	// Only for 64 bit architecture
-	syscall_keyctl   uintptr = 250
-	syscall_add_key  uintptr = 248
-	syscall_setfsgid uintptr = 123
-)
-
-const (
-	keyctlGetKeyringId keyctlCommand = iota
-	keyctlJoinSessionKeyring
-	keyctlUpdate
-	keyctlRevoke
-	keyctlChown
-	keyctlSetPerm
-	keyctlDescribe
-	keyctlClear
-	keyctlLink
-	keyctlUnlink
-	keyctlSearch
-	keyctlRead
-	keyctlInstantiate
-	keyctlNegate
-	keyctlSetReqKeyKeyring
-	keyctlSetTimeout
-	keyctlAssumeAuthority
-	keyctlGetPersistent
-)
-
-const (
-	// you can reference builtin keyrings this way
-	keySpecThreadKeyring      keyId = -1
-	keySpecProcessKeyring     keyId = -2
-	keySpecSessionKeyring     keyId = -3
-	keySpecUserKeyring        keyId = -4
-	keySpecUserSessionKeyring keyId = -5
-	keySpecGroupKeyring       keyId = -6
-	keySpecReqKeyAuthKey      keyId = -7
 )
 
 var debugSyscalls bool
 
 var count int
-
-type keyId int32
-type keyctlCommand int
-
-func (cmd keyctlCommand) String() string {
-	switch cmd {
-	case keyctlGetKeyringId:
-		return "keyctlGetKeyringId"
-	case keyctlJoinSessionKeyring:
-		return "keyctlJoinSessionKeyring"
-	case keyctlUpdate:
-		return "keyctlUpdate"
-	case keyctlRevoke:
-		return "keyctlRevoke"
-	case keyctlChown:
-		return "keyctlChown"
-	case keyctlSetPerm:
-		return "keyctlSetPerm"
-	case keyctlDescribe:
-		return "keyctlDescribe"
-	case keyctlClear:
-		return "keyctlClear"
-	case keyctlLink:
-		return "keyctlLink"
-	case keyctlUnlink:
-		return "keyctlUnlink"
-	case keyctlSearch:
-		return "keyctlSearch"
-	case keyctlRead:
-		return "keyctlRead"
-	case keyctlInstantiate:
-		return "keyctlInstantiate"
-	case keyctlNegate:
-		return "keyctlNegate"
-	case keyctlSetReqKeyKeyring:
-		return "keyctlSetReqKeyKeyring"
-	case keyctlSetTimeout:
-		return "keyctlSetTimeout"
-	case keyctlAssumeAuthority:
-		return "keyctlAssumeAuthority"
-	case keyctlGetPersistent:
-		return "keyctlGetPersistent"
-	}
-	panic("bad arg")
-}
-
-func add_key(keyType, keyDesc string, payload []byte, id int32) (int32, error) {
-	var (
-		err    error
-		errno  syscall.Errno
-		b1, b2 *byte
-		r1     uintptr
-		pptr   unsafe.Pointer
-	)
-
-	if b1, err = syscall.BytePtrFromString(keyType); err != nil {
-		return 0, err
-	}
-
-	if b2, err = syscall.BytePtrFromString(keyDesc); err != nil {
-		return 0, err
-	}
-
-	if len(payload) > 0 {
-		pptr = unsafe.Pointer(&payload[0])
-	}
-	r1, _, errno = syscall.Syscall6(syscall_add_key,
-		uintptr(unsafe.Pointer(b1)),
-		uintptr(unsafe.Pointer(b2)),
-		uintptr(pptr),
-		uintptr(len(payload)),
-		uintptr(id),
-		0)
-
-	if errno != 0 {
-		err = errno
-		return 0, err
-	}
-	return int32(r1), nil
-}
-
-func listKeys(id keyId) ([]keyId, error) {
-	var (
-		b1             []byte
-		size, sizeRead int
-	)
-
-	bsz := 4
-	b1 = make([]byte, 16*bsz)
-	size = len(b1)
-	sizeRead = size + 1
-	for sizeRead > size {
-		r1, _, errno := syscall.Syscall6(syscall_keyctl, uintptr(keyctlRead), uintptr(id), uintptr(unsafe.Pointer(&b1[0])), uintptr(size), 0, 0)
-		if errno != 0 {
-			return nil, errno
-		}
-
-		if sizeRead = int(r1); sizeRead > size {
-			b1 = make([]byte, sizeRead)
-			size = sizeRead
-			sizeRead++
-		} else {
-			size = sizeRead
-		}
-	}
-	keys := make([]keyId, size/bsz)
-	for i := range keys {
-		keys[i] = *((*keyId)(unsafe.Pointer(&b1[i*bsz])))
-	}
-
-	return keys, nil
-}
-
-// func newKeyring(id keyId) (*keyring, error) {
-// 	r1, _, errno := syscall.Syscall(syscall_keyctl, uintptr(keyctlGetKeyringId), uintptr(id), uintptr(1))
-// 	if errno != 0 {
-// 		return nil, errno
-// 	}
-
-// 	if id >= 0 {
-// 		id = keyId(r1)
-// 	}
-// 	return &keyring{id: id}, nil
-// }
-
-func (k Key) describeKeyId() ([]byte, error) {
-	var (
-		b1             []byte
-		size, sizeRead int
-	)
-
-	b1 = make([]byte, 64)
-	size = len(b1)
-	sizeRead = size + 1
-	for sizeRead > size {
-		r1, _, errno := syscall.Syscall6(syscall_keyctl, uintptr(keyctlDescribe), uintptr(keyId(k.KeyId)), uintptr(unsafe.Pointer(&b1[0])), uintptr(size), 0, 0)
-		if errno != 0 {
-			return nil, errno
-		}
-		if sizeRead = int(r1); sizeRead > size {
-			b1 = make([]byte, sizeRead)
-			size = sizeRead
-			sizeRead++
-		} else {
-			size = sizeRead
-		}
-	}
-
-	return b1[:size-1], nil
-}
-
-func keyctl_Read(id keyId, b *byte, size int) (int32, error) {
-	v1, _, errno := syscall.Syscall6(syscall_keyctl, uintptr(keyctlRead), uintptr(id), uintptr(unsafe.Pointer(b)), uintptr(size), 0, 0)
-	if errno != 0 {
-		return -1, errno
-	}
-
-	return int32(v1), nil
-}
-
-func keyctl_Unlink(id, ring keyId) error {
-	_, _, errno := syscall.Syscall(syscall_keyctl, uintptr(keyctlUnlink), uintptr(id), uintptr(ring))
-	if errno != 0 {
-		return errno
-	}
-	return nil
-}
-
-func keyctl_Link(id, ring keyId) error {
-	_, _, errno := syscall.Syscall(syscall_keyctl, uintptr(keyctlLink), uintptr(id), uintptr(ring))
-	if errno != 0 {
-		return errno
-	}
-	return nil
-}
-
-func keyctl_Get_Persistent(uid int, ring keyId) error {
-	// TODO the lookup for "GetPersistentId" is 17 but in reality it's 22
-	// I have no idea where this is set and who sets it...
-	// see https://github.com/torvalds/linux/blob/v5.4/include/uapi/linux/keyctl.h#L62
-	_, _, errno := syscall.Syscall(syscall_keyctl, uintptr(22), uintptr(uid), uintptr(ring))
-	if errno != 0 {
-		return errno
-	}
-	return nil
-}
-
-// func keyctl(cmd keyctlCommand, args ...uintptr) (r1 int32, r2 int32, err error) {
-// 	a := make([]uintptr, 6)
-// 	l := len(args)
-// 	if l > 5 {
-// 		l = 5
-// 	}
-// 	a[0] = uintptr(cmd)
-// 	for idx, v := range args[:l] {
-// 		a[idx+1] = v
-// 	}
-
-// 	debugSyscalls = true
-// 	fmt.Printf("%v: %v %v\n", syscall_keyctl, cmd, a[1:])
-
-// 	if debugSyscalls {
-// 		log.Printf("%v: %v %v\n", syscall_keyctl, cmd, a[1:])
-// 	}
-// 	v1, v2, errno := syscall.Syscall6(syscall_keyctl, a[0], a[1], a[2], a[3], a[4], a[5])
-// 	if errno != 0 {
-// 		err = errno
-// 		return
-// 	}
-
-// 	r1 = int32(v1)
-// 	r2 = int32(v2)
-// 	return
-// }
 
 var (
 	max         int
@@ -370,19 +115,45 @@ func main() {
 
 }
 
-type Key struct {
-	KeyId          int32
-	Valid          bool
-	Name           string
-	Type           string
-	Uid            string
-	Gid            string
-	Perms          string
-	String_Content string
-	Byte_Content   []byte
-	Comments       []string
-	Subkeys        []Key
-	size           int
+func (k *Key) Get() error {
+	// Perform a syscall keyctlread() to get the secret bytes
+	// of a key. Returns error if it can't read the key.
+
+	var (
+		b        []byte
+		err      error
+		sizeRead int
+	)
+
+	if k.size == 0 {
+		k.size = 512
+	}
+
+	size := k.size
+
+	b = make([]byte, int(size))
+	sizeRead = size + 1
+	for sizeRead > size {
+		r1, err := keyctl_Read(keyId(k.KeyId), &b[0], size)
+		if err != nil {
+			return err
+		}
+
+		if sizeRead = int(r1); sizeRead > size {
+			b = make([]byte, sizeRead)
+			size = sizeRead
+			sizeRead = size + 1
+		} else {
+			k.size = sizeRead
+		}
+	}
+
+	// Update the original keypointer
+	content := b[:k.size]
+	k.Byte_Content = content
+	k.String_Content = string(content)
+
+	return err
 }
 
 func hunter() {
@@ -505,28 +276,6 @@ func hunter() {
 	fmt.Println("Output saved to: ", output_path)
 }
 
-func (k *Key) populate_describe(bdesc []byte) error {
-	// Parse the response from the describekeyid syscall
-	// In the format of:
-	// 	user;1000;1000;3f1000000;myname
-	k.Valid = true // TODO do I need this here?
-	aReturn := strings.Split(string(bdesc), ";")
-	if len(aReturn) < 5 {
-		return fmt.Errorf("Something wrong parsing describekeyid results: %s", string(bdesc))
-
-	}
-
-	// Populate info from results
-	k.Type = aReturn[0]
-	k.Uid = aReturn[1]
-	k.Gid = aReturn[2]
-	k.Perms = aReturn[3]
-	k.Name = aReturn[4]
-
-	// TODO not very useful results
-	return nil
-}
-
 func (k *Key) populate_subkeys() (int, error) {
 	// Consume a keyid and run the syscall listkeys()
 	// Generates keyids for other keys part of the
@@ -568,45 +317,4 @@ func (k *Key) populate_subkeys() (int, error) {
 		}
 	}
 	return i, nil
-}
-
-func (k *Key) Get() error {
-	// Perform a syscall keyctlread() to get the secret bytes
-	// of a key. Returns error if it can't read the key.
-
-	var (
-		b        []byte
-		err      error
-		sizeRead int
-	)
-
-	if k.size == 0 {
-		k.size = 512
-	}
-
-	size := k.size
-
-	b = make([]byte, int(size))
-	sizeRead = size + 1
-	for sizeRead > size {
-		r1, err := keyctl_Read(keyId(k.KeyId), &b[0], size)
-		if err != nil {
-			return err
-		}
-
-		if sizeRead = int(r1); sizeRead > size {
-			b = make([]byte, sizeRead)
-			size = sizeRead
-			sizeRead = size + 1
-		} else {
-			k.size = sizeRead
-		}
-	}
-
-	// Update the original keypointer
-	content := b[:k.size]
-	k.Byte_Content = content
-	k.String_Content = string(content)
-
-	return err
 }
